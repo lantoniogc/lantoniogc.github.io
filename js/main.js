e1be42551a7ee071b1429545e7bf8043d0722627
Antonio García <algc1294@gmail.com>
2020-05-15 03:55:06 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementsByClassName('sidenav')[0].style.width = '100%';
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementsByClassName('sidenav')[0].style.width = '0';
+}
