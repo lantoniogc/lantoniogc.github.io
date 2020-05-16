546c3e9784f386158e326b213e200238f81f6dbc
Antonio García <algc1294@gmail.com>
2020-05-15 22:42:54 -0400

    Responsive for Tablets (About + Skills)

--- js/main.js
+++
@@ -1,9 +0,0 @@
-/* Open the sidenav */
-function openNav() {
-  document.getElementsByClassName('sidenav')[0].style.width = '100%';
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementsByClassName('sidenav')[0].style.width = '0';
-}
